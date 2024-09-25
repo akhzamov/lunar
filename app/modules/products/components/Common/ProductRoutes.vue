86d34dee60f49c55275aa8dfae2042f1ef155813
AbdulMalik Akhzamov <a.akhzamov@3205.team>
2024-09-25 13:06:48 +0500

    productRoutes

--- app/modules/products/components/Common/ProductRoutes.vue
+++ app/modules/products/components/Common/ProductRoutes.vue
@@ -1,25 +1,34 @@
 <script lang="ts" setup>
+	import Edit05 from "~/components/icon/Edit05.vue";
+	import Calendar from "~/components/icon/Calendar.vue";
+	import Image03 from "~/components/icon/Image03.vue";
+	import Colors from "~/components/icon/Colors.vue";
+
 	const route = useRoute();
 	const links = reactive([
 		{
 			id: 1,
 			name: "Основная Информация",
 			path: "edit",
+			icon: Edit05,
 		},
 		{
 			id: 2,
 			name: "Доступность",
 			path: "availability",
+			icon: Calendar,
 		},
 		{
 			id: 3,
 			name: "Медиа",
 			path: "media",
+			icon: Image03,
 		},
 		{
 			id: 4,
 			name: "Вариантность",
 			path: "variants",
+			icon: Colors,
 		},
 	]);
 </script>
@@ -35,9 +44,9 @@
 				class="relative h-[40px]"
 			>
 				<div
-					class="h-full flex items-center justify-start gap-2 b-bg border-y-[1px] border-r-[1px] border-c rounded-tr-md rounded-br-md pl-6 pr-3 z-[10]"
+					class="h-full flex items-center justify-start gap-2 b-bg border-y-[1px] border-r-[1px] border-c rounded-tr-md rounded-br-md pl-6 pr-3 z-[10] info"
 				>
-					<IconAward03 />
+					<component :is="link.icon" />
 					<span>{{ link.name }}</span>
 				</div>
 				<div
@@ -55,6 +64,10 @@
 
 	.router-link-active .line {
 		opacity: 1;
+	}
+
+	.router-link-active .info {
+		@apply text-c-primary-500;
 	}
 
 	.border-c {
